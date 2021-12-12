1171ada794272b77b7d5ab36fbc850f3ea0230b3
jorgelucas <jorgelucasl91@gmail.com>
2021-12-12 02:54:24 -0300

    [FEATURE] add error handler middleware

--- Estudos.BlobStorage/Program.cs
+++ Estudos.BlobStorage/Program.cs
@@ -1,4 +1,5 @@
 using Estudos.BlobStorage.Configurations;
+using Estudos.BlobStorage.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<ErrorHandlerMiddleware>();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
---
+++ Estudos.BlobStorage/Settings/ErrorHandlerMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using System.Text.Json;
+using Azure;
+
+namespace Estudos.BlobStorage.Settings;
+
+public class ErrorHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger _logger;
+
+    public ErrorHandlerMiddleware(RequestDelegate next, ILogger<ErrorHandlerMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception error)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+            string? message = null;
+            switch (error)
+            {
+                case RequestFailedException e:
+                    // Azure error
+                    response.StatusCode = e.Status;
+                    message = e.Message.Split(".")[0];
+                    break;
+                case KeyNotFoundException e:
+                    // not found error
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    break;
+                default:
+                    // unhandled error
+                    response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    break;
+            }
+
+            var result = JsonSerializer.Serialize(
+                new
+                {
+                    success = false,
+                    message = message ?? error?.Message
+                });
+            await response.WriteAsync(result);
+        }
+    }
+}
